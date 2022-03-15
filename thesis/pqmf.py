749a86dad267c416646ee0708716f6a6a58a5dd5
Václav Volhejn <vaclav.volhejn@gmail.com>
2022-03-15 16:02:41 +0100

    Clean up PQMF code and add tests

--- thesis/pqmf.py
+++ thesis/pqmf.py
@@ -1,167 +1,196 @@
-# -*- coding: utf-8 -*-
-# Copyright 2020 The Multi-band MelGAN Authors , Minh Nguyen (@dathudeptrai) and Tomoki Hayashi (@kan-bayashi)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ============================================================================
-#
-# Compatible with https://github.com/kan-bayashi/ParallelWaveGAN/blob/master/parallel_wavegan/layers/pqmf.py.
-# From https://github.com/TensorSpeech/TensorFlowTTS
+"""
+Pseudo Quadrature Mirror Filter bank for multi-band decomposition of a signal.
+As seen in RAVE and elsewhere.
 
-"""Multi-band MelGAN Modules."""
+Based on Antoine Caillon's implementation from RAVE:
+    https://github.com/caillonantoine/RAVE/blob/master/rave/pqmf.py
+and on the implementation from TensorFlowTTS:
+    https://github.com/TensorSpeech/TensorFlowTTS/blob/master/tensorflow_tts/models/mb_melgan.py
+TODO: check what their respective licenses require as a notice
+TODO: implement the faster version from RAVE if needed
+
+The TensorFlowTTS implementation yielded a reconstruction with audible artifacts
+so the code is based more on the RAVE implementation.
+"""
+
+import math
 
 import numpy as np
 import tensorflow as tf
-from scipy.signal import kaiser
+from scipy.signal import kaiser, kaiserord, firwin
+from scipy.optimize import fmin
+from einops import rearrange
+
+from tensorflow.python.ops.numpy_ops import np_config
+
+np_config.enable_numpy_behavior()
 
 
-def design_prototype_filter(taps=62, cutoff_ratio=0.15, beta=9.0):
-    """Design prototype filter for PQMF.
-    This method is based on `A Kaiser window approach for the design of prototype
-    filters of cosine modulated filterbanks`_.
-    Args:
-        taps (int): The number of filter taps.
-        cutoff_ratio (float): Cut-off frequency ratio.
-        beta (float): Beta coefficient for kaiser window.
-    Returns:
-        ndarray: Impulse response of prototype filter (taps + 1,).
-    .. _`A Kaiser window approach for the design of prototype filters of cosine modulated filterbanks`:
-        https://ieeexplore.ieee.org/abstract/document/681427
+def reverse_half(x):
+    mask = np.ones_like(x)
+    mask[..., ::2, 1::2] = -1
+
+    return x * mask
+
+
+def center_pad_next_pow_2(x):
     """
-    # check the arguments are valid
-    assert taps % 2 == 0, "The number of taps mush be even number."
-    assert 0.0 < cutoff_ratio < 1.0, "Cutoff ratio must be > 0.0 and < 1.0."
+    Pad a Tensor of shape [a, b] to shape [a, b']
+    where b' is the smallest power of two not smaller than b.
+    """
+    next_2 = 2 ** math.ceil(math.log2(x.shape[-1]))
+    pad = next_2 - x.shape[-1]
+    # return nn.functional.pad(x, (pad // 2, pad // 2 + int(pad % 2)))
+    return tf.pad(x, [(0, 0), (pad // 2, pad // 2 + int(pad % 2))])
 
-    # make initial filter
-    omega_c = np.pi * cutoff_ratio
-    with np.errstate(invalid="ignore"):
-        h_i = np.sin(omega_c * (np.arange(taps + 1) - 0.5 * taps)) / (
-            np.pi * (np.arange(taps + 1) - 0.5 * taps)
-        )
-    # fix nan due to indeterminate form
-    h_i[taps // 2] = np.cos(0) * cutoff_ratio
 
-    # apply kaiser window
-    w = kaiser(taps + 1, beta)
-    h = h_i * w
+def get_qmf_bank(h, n_band):
+    """
+    Modulates an input protoype filter into a bank of
+    cosine modulated filters
+    Parameters
+    ----------
+    h: torch.Tensor
+        prototype filter
+    n_band: int
+        number of sub-bands
+    """
+    k = tf.range(n_band).reshape(-1, 1)
+    N = h.shape[-1]
+    t = tf.range(-(N // 2), N // 2 + 1)
 
+    p = (-1) ** k * math.pi / 4
+
+    mod = tf.cos((2 * k + 1) * math.pi / (2 * n_band) * t + p)
+    hk = 2 * h * mod
+
+    return hk.astype(tf.float32)
+
+
+def kaiser_filter(wc, atten, N=None):
+    """
+    Computes a kaiser lowpass filter
+    Parameters
+    ----------
+    wc: float
+        Angular frequency
+
+    atten: float
+        Attenuation (dB, positive)
+    """
+    N_, beta = kaiserord(atten, wc / np.pi)
+
+    N_ = 2 * (N_ // 2) + 1
+    N = N if N is not None else N_
+    h = firwin(N, wc, window=("kaiser", beta), scale=False, nyq=np.pi)
     return h
 
 
-class MultiBandMelGANGeneratorConfig:
-    """Initialize Multi-band MelGAN Generator Config."""
+def loss_wc(wc, atten, M, N):
+    """
+    Computes the objective described in https://ieeexplore.ieee.org/document/681427
+    """
+    h = kaiser_filter(wc, atten, N)
+    g = np.convolve(h, h[::-1], "full")
+    g = abs(g[g.shape[-1] // 2 :: 2 * M][1:])
+    return np.max(g)
 
-    def __init__(self, **kwargs):
-        self.subbands = kwargs.pop("subbands", 4)
-        self.taps = kwargs.pop("taps", 62)
-        self.cutoff_ratio = kwargs.pop("cutoff_ratio", 0.142)
-        self.beta = kwargs.pop("beta", 9.0)
+
+def get_prototype(atten, M, N=None):
+    """
+    Given an attenuation objective and the number of bands
+    returns the corresponding lowpass filter
+    """
+    wc = fmin(lambda w: loss_wc(w, atten, M, N), 1 / M, disp=0)[0]
+    return kaiser_filter(wc, atten, N)
 
 
 class PQMF(tf.keras.layers.Layer):
-    """PQMF module."""
+    """
+    Pseudo Quadrature Mirror Filter multiband decomposition / reconstruction
+    Parameters
+    ----------
+    attenuation: int
+        Attenuation of the rejected bands (dB, 80 - 120)
+    n_bands: int
+        Number of bands, must be a power of 2 if the polyphase implementation
+        is needed (currently not implemented)
+    """
 
-    def __init__(self, config, **kwargs):
-        """Initilize PQMF module.
-        Args:
-            config (class): MultiBandMelGANGeneratorConfig
-        """
-        super().__init__(**kwargs)
-        subbands = config.subbands
-        taps = config.taps
-        cutoff_ratio = config.cutoff_ratio
-        beta = config.beta
+    def __init__(self, attenuation, n_bands):
+        super().__init__()
+        h = get_prototype(attenuation, n_bands)
 
-        # define filter coefficient
-        h_proto = design_prototype_filter(taps, cutoff_ratio, beta)
-        h_analysis = np.zeros((subbands, len(h_proto)))
-        h_synthesis = np.zeros((subbands, len(h_proto)))
-        for k in range(subbands):
-            h_analysis[k] = (
-                2
-                * h_proto
-                * np.cos(
-                    (2 * k + 1)
-                    * (np.pi / (2 * subbands))
-                    * (np.arange(taps + 1) - (taps / 2))
-                    + (-1) ** k * np.pi / 4
-                )
-            )
-            h_synthesis[k] = (
-                2
-                * h_proto
-                * np.cos(
-                    (2 * k + 1)
-                    * (np.pi / (2 * subbands))
-                    * (np.arange(taps + 1) - (taps / 2))
-                    - (-1) ** k * np.pi / 4
-                )
-            )
+        self.h = tf.convert_to_tensor(h).astype(tf.float32)
+        self.hk = get_qmf_bank(h, n_bands)
+        self.hk = center_pad_next_pow_2(self.hk)
+        self.hk = rearrange(self.hk, "filters taps -> taps 1 filters")
 
-        # [subbands, 1, taps + 1] == [filter_width, in_channels, out_channels]
-        analysis_filter = np.expand_dims(h_analysis, 1)
-        analysis_filter = np.transpose(analysis_filter, (2, 1, 0))
+        self.n_bands = n_bands
 
-        synthesis_filter = np.expand_dims(h_synthesis, 0)
-        synthesis_filter = np.transpose(synthesis_filter, (2, 1, 0))
-
-        # filter for downsampling & upsampling
-        updown_filter = np.zeros((subbands, subbands, subbands), dtype=np.float32)
-        for k in range(subbands):
+        updown_filter = np.zeros((n_bands, n_bands, n_bands), dtype=np.float32)
+        for k in range(n_bands):
             updown_filter[0, k, k] = 1.0
-
-        self.subbands = subbands
-        self.taps = taps
-        self.analysis_filter = analysis_filter.astype(np.float32)
-        self.synthesis_filter = synthesis_filter.astype(np.float32)
         self.updown_filter = updown_filter.astype(np.float32)
 
-    @tf.function(
-        experimental_relax_shapes=True,
-        input_signature=[tf.TensorSpec(shape=[None, None, 1], dtype=tf.float32)],
-    )
+        self.taps = int(self.h.shape[0])
+
     def analysis(self, x):
-        """Analysis with PQMF.
-        Args:
-            x (Tensor): Input tensor (B, T, 1).
-        Returns:
-            Tensor: Output tensor (B, T // subbands, subbands).
         """
-        x = tf.pad(x, [[0, 0], [self.taps // 2, self.taps // 2], [0, 0]])
-        x = tf.nn.conv1d(x, self.analysis_filter, stride=1, padding="VALID")
-        x = tf.nn.conv1d(x, self.updown_filter, stride=self.subbands, padding="VALID")
+        PQMF bank analysis.
+        :param x: a Tensor of shape [batch, n_samples, 1]
+        :return: a Tensor of shape [batch, n_samples / n_bands, n_bands]
+        """
+        tf.ensure_shape(x, [None, None, 1])
+
+        if self.n_bands == 1:
+            return x
+
+        x = tf.nn.conv1d(x, self.hk, stride=self.hk.shape[2], padding="SAME")
+        x = reverse_half(x)
+
+        tf.ensure_shape(x, [None, None, self.n_bands])
         return x
 
-    @tf.function(
-        experimental_relax_shapes=True,
-        input_signature=[tf.TensorSpec(shape=[None, None, None], dtype=tf.float32)],
-    )
     def synthesis(self, x):
-        """Synthesis with PQMF.
-        Args:
-            x (Tensor): Input tensor (B, T // subbands, subbands).
-        Returns:
-            Tensor: Output tensor (B, T, 1).
         """
+        PQMF bank synthesis.
+        :param x: a Tensor of shape [batch, n_samples / n_bands, n_bands]
+            produced by self.analysis(x)
+        :return: a Tensor of shape [batch, n_samples, 1],
+            a reconstruction of the original x
+        """
+        tf.ensure_shape(x, [None, None, self.n_bands])
+
+        if self.n_bands == 1:
+            return x
+
+        x = reverse_half(x)
+
+        # First, upsample x to the original n_samples by adding zeroes so that each value
+        # in `x` is `n_bands` positions apart, e.g. for n_bands=4 we'd have
+        # x[i,:,j] <- [x[i,0,j] 0 0 0 x[i,1,j] 0 0 0...]
         x = tf.nn.conv1d_transpose(
             x,
-            self.updown_filter * self.subbands,
-            strides=self.subbands,
+            self.updown_filter * self.n_bands,
+            strides=self.n_bands,
             output_shape=(
                 tf.shape(x)[0],
-                tf.shape(x)[1] * self.subbands,
-                self.subbands,
+                tf.shape(x)[1] * self.n_bands,
+                self.n_bands,
             ),
         )
-        x = tf.pad(x, [[0, 0], [self.taps // 2, self.taps // 2], [0, 0]])
 
-        return tf.nn.conv1d(x, self.synthesis_filter, stride=1, padding="VALID")
+        # We need to transpose since the convolution is now going
+        # from n_bands channels to one
+        hk = rearrange(self.hk, "taps 1 filters -> taps filters 1")
+
+        hk = tf.reverse(hk, [0])
+
+        taps = hk.shape[0]
+
+        x = tf.pad(x, [[0, 0], [taps // 2, taps // 2], [0, 0]])
+        y = tf.nn.conv1d(x, hk, stride=1, padding="VALID")[..., 1:, :]
+
+        tf.ensure_shape(y, [None, None, 1])
+        return y
---
+++ thesis/pqmf_test.py
@@ -0,0 +1,47 @@
+import einops
+import tensorflow as tf
+from ddsp.losses import SpectralLoss
+from ddsp.colab.colab_utils import audio_bytes_to_np
+
+from thesis.pqmf import PQMF
+
+
+def get_loss(target_audio, audio):
+    loss_class = SpectralLoss()
+    loss = loss_class(tf.convert_to_tensor(target_audio), tf.convert_to_tensor(audio))
+    return float(loss)
+
+
+def get_audio_fixture():
+    input_f = open("../fixtures/violin_1s.wav", "rb")
+    wav_bytes = input_f.read()
+    audio = audio_bytes_to_np(wav_bytes)
+    return audio
+
+
+class PQMFTest(tf.test.TestCase):
+    def test_pqmf(self):
+        audio = get_audio_fixture()
+        audio = tf.convert_to_tensor(audio)
+
+        # Split into two halves to have a batch size of more than 1
+        batch_size = 2
+        audio = einops.rearrange(audio, "(b t) -> b t 1", b=batch_size)
+        n_samples = audio.shape[1]
+
+        bands = 4
+        attenuation = 100
+
+        pqmf = PQMF(attenuation=attenuation, n_bands=bands)
+
+        analyzed = pqmf.analysis(audio)
+        self.assertEqual(
+            analyzed.shape.as_list(), [batch_size, n_samples // bands, bands]
+        )
+
+        synthesized = pqmf.synthesis(analyzed)
+        self.assertEqual(synthesized.shape.as_list(), [batch_size, n_samples, 1])
+
+        # The reconstructed audio should be close enough to the original.
+        # The actual loss value should be <=0.1, so the threshold 1.0 is lenient.
+        self.assertLess(get_loss(audio, synthesized), 1.0)
