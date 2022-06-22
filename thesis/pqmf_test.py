c3ccc3c1735eb8c80d7377e4010c9484bda8048f
Václav Volhejn <vaclav.volhejn@gmail.com>
2022-06-22 16:16:04 +0200

    Misc fixes

--- thesis/pqmf_test.py
+++ thesis/pqmf_test.py
@@ -3,9 +3,33 @@ import os
 import einops
 import tensorflow as tf
 from ddsp.losses import SpectralLoss
-from ddsp.colab.colab_utils import audio_bytes_to_np
+# from ddsp.colab.colab_utils import audio_bytes_to_np
+import note_seq
 
 from thesis.pqmf import PQMFBank
+
+
+def audio_bytes_to_np(wav_data,
+                      sample_rate=16000,
+                      normalize_db=0.1,
+                      mono=True):
+  """Convert audio file data (in bytes) into a numpy array using Pydub.
+
+  Args:
+    wav_data: A byte stream of audio data.
+    sample_rate: Resample recorded audio to this sample rate.
+    normalize_db: Normalize the audio to this many decibels. Set to None to skip
+      normalization step.
+    mono: Force stereo signals to single channel. If false, output can one or
+      two channels depending on the source signal.
+
+  Returns:
+    An array of the recorded audio at sample_rate, shape [channels, time].
+  """
+  return note_seq.audio_io.wav_data_to_samples_pydub(
+      wav_data=wav_data, sample_rate=sample_rate, normalize_db=normalize_db,
+      num_channels=1 if mono else None)
+
 
 
 def get_loss(target_audio, audio):
